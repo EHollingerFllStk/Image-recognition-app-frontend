d495cb6e02b459fa1904565823fd4b895f3e0912
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2023-05-10 16:32:57 -0400

    store state of prediction and imageURL for prediction - show output data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import Container from "react-bootstrap/Container";
+import { useState } from "react";
+import { Container, Stack } from "react-bootstrap";
 
 import InputImage from "./InputImage";
-
+import Output from "./Output";
 
 function App() {
+  const [outputs, setOutputs] = useState([]);
+  const [imageToPredict, setImageToPredict] = useState("");
+
   return (
     <div>
       <Container>
         <div className="mt-3" />
-        <InputImage />
+        <Stack gap={2}>
+          <InputImage setOutputs={setOutputs}
+          setImageToPredict={setImageToPredict} />
+          <Output outputs={outputs} imageToPredict={imageToPredict} />
+        </Stack>
       </Container>
     </div>
   );
